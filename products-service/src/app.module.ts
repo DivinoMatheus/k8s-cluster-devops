e526ad09a8b9622c5dd3815ec22a53dbc136f292
Divino Matheus <divinomatheusyg@gmail.com>
2024-04-13 15:25:59 -0300

    chore: creates a health endpoint

--- products-service/src/app.module.ts
+++ products-service/src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { ProductsController } from './presentation/products.controller';
 import { DatabaseModule } from './infrastructure/database/database.module';
 import { UseCaseProviders } from './domain/usecases/usecase.providers';
+import { HealthController } from './presentation/health.controller';
+import { TerminusModule } from '@nestjs/terminus';
 
 
 @Module({
-  imports: [DatabaseModule],
-  controllers: [ProductsController],
+  imports: [TerminusModule, DatabaseModule],
+  controllers: [ProductsController, HealthController],
   providers: [...UseCaseProviders],
 })
 export class AppModule {}
